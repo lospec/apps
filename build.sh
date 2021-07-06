6f81637722c30a370bfae9f20968c9caaebfe16a
skeddles <skeddtemp@gmail.com>
2021-07-06 11:30:23 -0400

    why isnt this working

--- build.sh
+++ build.sh
@@ -5,10 +5,13 @@ mkdir ./build
 
 #PIXEL EDITOR
 #git clone https://github.com/lospec/pixel-editor
+cd hello
 git clone --branch static-site-conversion https://github.com/lospec/pixel-editor
 cd pixel-editor
 npm install
 npm run build
-echo \ninstalled pixel editor\n
+echo 
+echo installed pixel editor
+echo
 
 cp -a ./build ../build/pixel-editor
