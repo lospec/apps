86008882265b701e6f01654feb9f4aea81ad17d7
skeddles <skeddtemp@gmail.com>
2021-07-06 10:53:32 -0400

    added build folder

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-#download 
+#setup
+mkdir ./build
+
+
+
+#PIXEL EDITOR
 git clone https://github.com/lospec/pixel-editor
 cd pixel-editor
 npm install
 #npm run build
-echo installed pixel editor
+echo \ninstalled pixel editor\n
+
+cp -a ./build ../build/pixel-editor
