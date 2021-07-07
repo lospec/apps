473a8d32f61953695579440016511f101e0151d9
skeddles <skeddtemp@gmail.com>
2021-07-06 21:15:19 -0400

    reverted to using main pixel editor branch

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ set -x #echo on
 mkdir ./build
 
 #PIXEL EDITOR
-#git clone https://github.com/lospec/pixel-editor
-git clone --branch static-site-conversion https://github.com/lospec/pixel-editor
+git clone https://github.com/lospec/pixel-editor
 cd pixel-editor
 npm install
 npm run build
